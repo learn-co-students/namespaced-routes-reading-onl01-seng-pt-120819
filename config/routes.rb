7ca976d24d002508ac249d45066544483c9124dd
Robert Shields <robert.shieldsiv@gmail.com>
2020-05-18 22:05:29 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  #fdffasdf
+  
+
   resources :authors, only: %i[show index] do
     resources :posts, only: %i[show index new edit]
   end
 
   resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
+  scope '/admin', module: 'admin' do
+    resources :stats, only: [:index]
+  end
 
   root 'posts#index'
 end
